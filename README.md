dc63a131980b23028760e2ca72e802182116f61e
Diego Alvarez <dialvarezs@gmail.com>
2018-08-27 21:36:23 -0300

    Approach change: simulations based on original data. - Now N "simulations" are done with the same number of sequences than the input data, with a number of mutations randomly generated from a normal distribution that fits the mutations per sequence from the original data. - Implemented parallelism using multiprocessing's Pool to speed up calculations with better resource usage. Uses all threads available. - Improved documentation from main script.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## Scripts to simulate tandem generation in IGH sequences
+# Scripts to simulate tandem generation in IGH sequences
 
 - `get_references.py`  finds refereces by name in germline and output them to a single fasta file.
 
@@ -6,11 +6,11 @@
 
 - `tandem_generation.py` simulates mutations and collect information about clusters of mutations with size 2 or higher. Outputs a table with the information for each cluster found
 
----
-### Example of use
+
+## Usage example
 - Install dependencies: `pip install -r requirements.txt`
 - Get references: `./get_references.py`
 - Get probabilities: `./get_probabilities.py`
-- Generate tandem information (1000 sequences): `./tandem_generation.py mutation_info/IGHV1-2-02.tsv fasta/references.fasta mutation_info/mutations_by_seq.txt 1000 mutation_info/IGHV1-2-02.tsv`
+- Generate tandem information (1000 simulations): `./tandem_generation.py mutation_info/IGHV1-2-02.tsv fasta/references.fasta mutation_info/mutations_by_seq.txt 1000 mutation_info/IGHV1-2-02.tsv`
 
-- To view the notebooks `jupyter` is also needed. If `jupyterlab` is used, need the plotly extension too.
+- To view the notebooks `jupyter` must be installed. If `jupyterlab` is used, [jupyterlabs' plotly extension](https://github.com/jupyterlab/jupyter-renderers/tree/master/packages/plotly-extension) is needed too.
--- tandem_generation.py
+++ tandem_generation.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import re
 import sys
+from multiprocessing import Pool
+from itertools import chain
 from Bio import SeqIO
 import pandas as pd
 import numpy as np
@@ -22,33 +24,48 @@ def main():
     # load data
     references = {x.id: x for x in SeqIO.parse(ref_file, 'fasta')}
     with open(mutations_by_seq) as f:
-        max_mutations_byseq = {x.split('\t')[0]: max([int(y) for y in x.split('\t')[1].split(',')]) for x in f.read().splitlines()}
+        for line in f.read().splitlines():
+            line = line.split('\t')
+            if line[0] == target_name:
+                mutations_by_seq = [int(x) for x in line[1].split(',')]
+                break
 
     m_info = pd.read_csv(mutation_info)
     ref = references[target_name]
-    max_mutations = max_mutations_byseq[target_name]
     n_size = len(str(n))
 
-    tandem_info = []
-    # generate sequences with a fixed number of mutations, from 2 to max_mutations
-    for i in range(2, max_mutations + 1):
-        for j in range(n):
-            mutations = generate_mutations(ref, m_info.mutation_probability, i)
-            for v in get_1d_clusters(sorted(mutations)):
-                tandem = ''.join(mutations[m] for m in v)
-                t_size = len(v)
-                # check if a stop codon is produced
-                mutated_subseq = re.sub('[.]+', tandem, get_context(ref, v[0], 2, t_size))
-                stop = any(s in mutated_subseq for s in STOP_CODONS)
+    # runs in parallel n simulations with len(mutations_by_seq) sequences
+    with Pool() as p:
+        tandem_info = p.starmap(run_simulation, ((i, m_info, ref, mutations_by_seq, n_size) for i in range(n)))
 
-                tandem_info.append([i, str(j+1).zfill(n_size), v[0], ref[v[0]: v[0]+t_size].seq, tandem, t_size, stop])
+    # flatten list of lists to a single list
+    tandem_info = list(chain.from_iterable(tandem_info))
 
-    tandem_df = pd.DataFrame(tandem_info, columns=['mutations_byseq', 'seq_id', 'pos', 'ref', 'alt', 'size', 'stop_codon'])
+    tandem_df = pd.DataFrame(tandem_info, columns=['simulation', 'mutations_byseq', 'seq_id', 'pos', 'ref', 'alt', 'size', 'stop_codon'])
     tandem_df.to_csv(outfile + '.gz', compression='gzip', sep='\t', index=False)
 
 
-# generate a set of mutations on a sequence based on a vector of probabilities
+# "main" operation wrapped in a function, to being able to paralelize it
+def run_simulation(sim_id, m_info, ref, mutations_by_seq, n_size):
+    n_mutations = random_fit_nonnegative(mutations_by_seq, len(mutations_by_seq))
+    info_list = []
+    for j, k in enumerate(n_mutations):
+        mutations = generate_mutations(ref, m_info.mutation_probability, k)
+        for v in get_1d_clusters(sorted(mutations)):
+            tandem = ''.join(mutations[m] for m in v)
+            t_size = len(v)
+
+            mutated_subseq = re.sub('[.]+', tandem, get_context(ref, v[0], 2, t_size))
+            # check if a stop codon is produced
+            stop = any(s in mutated_subseq for s in STOP_CODONS)
+
+            info_list.append([sim_id + 1, k, str(j + 1).zfill(n_size), v[0], ref[v[0]: v[0] + t_size].seq, tandem, t_size, stop])
+
+    return info_list
+
+
 def generate_mutations(ref, mutation_probabilities, n):
+    """Generates a set of mutations on a sequence based on a vector of probabilities."""
     seq_len = len(ref)
     # working with numpy arrays to speed up the proccess
     positions = np.arange(0, seq_len)
@@ -60,7 +77,7 @@ def generate_mutations(ref, mutation_probabilities, n):
         while len(mutation_candidates) == 0:
             random = np.random.rand(seq_len)
             mutation_candidates = positions[random <= mutation_prob]
-        # select randomly one of the mutation candidates and generate a mutation
+        # randomly select one of the mutation candidates and generate a mutation
         pos = int(np.random.choice(mutation_candidates))
         mutated_base = mutate(ref[pos])
         mutations[pos] = mutated_base
@@ -68,24 +85,65 @@ def generate_mutations(ref, mutation_probabilities, n):
     return mutations
 
 
-# choose randomly a different base than the one from the argument
 def mutate(base):
+    """Chooses randomly a different base than the one from the argument."""
     return np.random.choice([x for x in "ACGT" if x != base])
 
 
-# gets context from a sequence at a given position
-# n defines how many bases are needed by each side
-# s defines the size of the central region
 def get_context(seq, pos, n=1, s=1):
-    return ''.join(['.' if pos <= n < pos+s else seq[n] if 0 <= n < len(seq) else '' for n in range(pos-n, pos+n+s)])
+    """
+    Gets context from a sequence at a given position.
+    :param seq: the sequence
+    :param pos: target position
+    :param n: context size (each side)
+    :param s: size of the target region, displaces the position of right context
+    :return: a string matching [ACTG]*[.]+[ACTG]*
+    """
+    context = []
+    for n in range(pos-n, pos+n+s):
+        if pos <= n < pos+s:
+            # target region, will be represented with dots
+            context.append('.')
+        elif 0 <= n < len(seq):
+            # context region, if it is in sequence boundaries
+            context.append(seq[n])
+        # else (positions outside sequence) => nothing
+
+    return ''.join(context)
 
 
-# given a array, finds clusters of elmeents on it.
-# stepsize defines the minimun distance between elements
 def get_1d_clusters(data, stepsize=1):
+    """
+    Finds clusters of elements in an array. Returns only clusters with more than 1 element.
+    :param data: input array
+    :param stepsize: minimun distance between elements to cluster them
+    :return: a list of arrays with the clusters found
+    """
     consecutive = np.split(data, np.where(np.diff(data) != stepsize)[0]+1)
-    # only return cluters with size > 1
     return [x for x in consecutive if len(x) > 1]
+
+
+def random_fit_nonnegative(values, n):
+    """
+    Generates n random values using a normal distribution fitted from values used as argument.
+    Returns only non-negative values.
+    :param values: array/list to use as model fot the random data
+    :param n: number of random elements to return
+    :returns: an array of n random non-negative numbers
+    """
+    values = np.array(values)
+    mean = np.mean(values)
+    sd = np.std(values)
+
+    random_values = np.empty(0)
+    offset = 0.05  # 5% offset to compa values less than 0
+    while len(random_values) < n:
+        random_values = np.round(np.random.normal(mean, sd, round(n*(1+offset))))
+        random_values = random_values[random_values >= 0]
+        offset *= 2  # If the while loop check fail, next time will try with a larger offset
+
+    # slice n first elements and shape the array to int
+    return random_values[:n].astype('int')
 
 
 if __name__ == '__main__':

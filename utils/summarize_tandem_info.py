33a59902f2f087f93eda94e64b07037be4e7e5f3
Diego Alvarez <dialvarezs@gmail.com>
2018-12-03 08:33:14 -0300

    Summarize for tandems with stop codons as well

--- utils/summarize_tandem_info.py
+++ utils/summarize_tandem_info.py
@@ -3,7 +3,7 @@
 import re
 from multiprocessing import Pool
 from glob import glob
-from os import mkdir, path
+from os import path, makedirs
 import pandas as pd
 
 INPUT_DIR = "tandem_info/simulated"
@@ -11,11 +11,8 @@ OUTPUT_DIR = "tandem_info_summarized"
 
 
 def main():
-    if not path.isdir(OUTPUT_DIR):
-        mkdir(OUTPUT_DIR)
-
-    with Pool(4) as p:
-        p.map(summarize, sorted(glob(path.join(INPUT_DIR, "*2/*.tsv.gz"))))
+    with Pool() as p:
+        p.map(summarize, sorted(glob(path.join(INPUT_DIR, "*/*.tsv.gz"))))
 
 
 def summarize(filename):
@@ -23,22 +20,30 @@ def summarize(filename):
     name = re.sub(r'^.*/|\..*$', '', filename)
     method = filename.split('/')[-2]
 
-    if not path.isdir(path.join(OUTPUT_DIR, name)):
-        mkdir(path.join(OUTPUT_DIR, name))
+    makedirs(path.join(OUTPUT_DIR, method, name), exist_ok=True)
 
     df = pd.read_table(filename)
 
-    # count by simulation/size
-    df_gr = pd.DataFrame(df.groupby(['simulation', 'size']).size()).rename(columns={0: 'n'})
-    df_gr.to_csv(path.join(OUTPUT_DIR, name, "count_sim_size_{}.tsv".format(method)), sep='\t')
+    for v in [False, True]:
+        suffix = '_stop' if v is True else ''
+        subset = df[df.stop_codon == v] 
 
-    # count by simulation/mutation
-    df_gr = pd.DataFrame(df.groupby(['simulation', 'ref', 'alt', 'size']).size()).rename(columns={0: 'n'})
-    df_gr.to_csv(path.join(OUTPUT_DIR, name, "count_sim_mut_{}.tsv".format(method)), sep='\t')
+        if len(subset) > 0:
+            # count by simulation/size
+            df_gr = pd.DataFrame(subset.groupby(['simulation', 'size']).size()).rename(columns={0: 'n'})
+            df_gr.to_csv(path.join(OUTPUT_DIR, method, name, "count_sim_size{}.tsv".format(suffix)), sep='\t')
 
-    # count by simulation/sequence
-    df_gr = pd.DataFrame(df.groupby(['simulation', 'seq_id']).size()).rename(columns={0: 'n'})
-    df_gr.to_csv(path.join(OUTPUT_DIR, name, "count_sim_seq_{}.tsv".format(method)), sep='\t')
+            # count by simulation/mutation
+            df_gr = pd.DataFrame(subset.groupby(['simulation', 'ref', 'alt', 'size']).size()).rename(columns={0: 'n'})
+            df_gr.to_csv(path.join(OUTPUT_DIR, method, name, "count_sim_mut{}.tsv".format(suffix)), sep='\t')
+
+            # count by simulation/sequence
+            df_gr = pd.DataFrame(subset.groupby(['simulation', 'seq_id']).size()).rename(columns={0: 'n'})
+            df_gr.to_csv(path.join(OUTPUT_DIR, method, name, "count_sim_seq{}.tsv".format(suffix)), sep='\t')
+
+            # count by simulation/position
+            df_gr = pd.DataFrame(subset.groupby(['simulation', 'pos', 'ref', 'size']).size()).rename(columns={0: 'n'})
+            df_gr.to_csv(path.join(OUTPUT_DIR, method, name, "count_sim_pos{}.tsv".format(suffix)), sep='\t')
 
 
 if __name__ == '__main__':
